b3af078e12751e987b6e5c34653d5d0be6e394c2
titouan <titouan.creach@eegle.io>
2018-03-26 16:21:28 +0200

    add app

---
+++ {{ cookiecutter.repo_name }}/app/src/components/Hello.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Hello World!</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ {{ cookiecutter.repo_name }}/app/src/index.js
@@ -0,0 +1,28 @@
+{%- macro camel_case(str) -%}
+{{ str.title().replace("_", "") }}
+{%- endmacro -%}
+
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import './style/style.scss';
+
+import Hello from '@/components/Hello.vue';
+
+const {{ camel_case(cookiecutter.app_code) }} = {
+  initUI(/* app */) {
+    Vue.use(Vuetify);
+    this.vm = new Vue({
+      components: {
+        Hello
+      },
+      render: h => h(Hello)
+    }).$mount('#{{ cookiecutter.app_code }}-mntpt');
+  },
+
+  exitUI(/* app */) {
+    this.vm.$destroy();
+  }
+};
+
+export default {{ camel_case(cookiecutter.app_code) }};
